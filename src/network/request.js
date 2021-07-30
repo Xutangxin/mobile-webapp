22cad36ebfb2d938d1daafbfd50caa128ed10fb4
Xutangxin <8554884+xu-tangxin@user.noreply.gitee.com>
2021-07-30 20:35:19 +0800

    11st commit 设置网络拦截 添加网络加载进度条

--- src/network/request.js
+++ src/network/request.js
@@ -1,8 +1,22 @@
 import axios from 'axios';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+
 
 export function request(config) {
     let instance = axios.create({
         baseURL: 'https://ku.qingnian8.com/dataApi/qingKu',
+    });
+
+    //网络加载进度条及拦截器配置
+    instance.interceptors.request.use(config => {
+        NProgress.start();
+        return config;
+    });
+
+    instance.interceptors.response.use(res => {
+        NProgress.done();
+        return res;
     });
 
     return instance(config);
